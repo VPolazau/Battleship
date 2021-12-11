6eac49fb1e8bd104bd30ae54814557549219033c
Victor <VPolazau@gmail.com>
2021-12-11 11:56:58 +0300

    clear btn

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const inputBtn = document.querySelector('button.btn-dark')
+const divInput = document.querySelector('.col-auto')
 const addShips = document.querySelector('button.btn-secondary')
 //
 //
@@ -17,6 +18,9 @@ class Store {
 
   get mapId() {}
   set mapId(id) {
+    if (this.map[id]) {
+      this.map[id] = false
+    }
     this.map[id] = true
   }
 }
@@ -24,35 +28,43 @@ const store = new Store()
 
 class BuildFields {
   createFields(height, width) {
-    const field = document.createElement('div')
-    field.id = '2_player'
-    field.classList.add('btn-group-vertical')
-    this.createRows(height, width).forEach(rows =>
-      field.insertAdjacentElement('beforeend', rows)
+    const field1 = document.createElement('div')
+    field1.id = '1_player'
+    field1.classList.add('btn-group-vertical')
+    this.createRows(height, width, 1).forEach(rows =>
+      field1.insertAdjacentElement('beforeend', rows)
     )
 
-    battlefields.insertAdjacentElement('beforeend', field)
+    const field2 = document.createElement('div')
+    field2.id = '2_player'
+    field2.classList.add('btn-group-vertical')
+    this.createRows(height, width, 2).forEach(rows =>
+      field2.insertAdjacentElement('beforeend', rows)
+    )
+
+    battlefields.insertAdjacentElement('beforeend', field1)
+    battlefields.insertAdjacentElement('beforeend', field2)
   }
 
-  createRows(h, width) {
+  createRows(h, width, player) {
     const rows = []
 
     for (let index = 0; index < h; index++) {
       const row = document.createElement('div')
 
       row.classList.add('btn-group')
-      this.createColoms(width, index).forEach(cols =>
+      this.createColoms(width, index, player).forEach(cols =>
         row.insertAdjacentElement('beforeend', cols)
       )
 
       rows.push(row)
     }
 
-    addDelBtn(rows, true)
+    addDelBtn(rows, true, 'Выставил')
     return rows
   }
 
-  createColoms(w, id) {
+  createColoms(w, id, player) {
     const cols = []
 
     for (let index = 0; index < w; index++) {
@@ -62,11 +74,12 @@ class BuildFields {
       col.classList.add('btn-light')
 
       const svg = document.createElement('i')
-      svg.id = `${id}${index}`
+      svg.id = `${player}_${id}${index}`
       svg.classList.add('far')
       svg.classList.add('fa-square')
       svg.classList.add('fa-2x')
       svg.classList.add('clear')
+      svg.classList.add('placed')
 
       col.insertAdjacentElement('beforeend', svg)
       cols.push(col)
@@ -97,6 +110,8 @@ function btnCreate(e) {
 
   buildFields.createFields(size.height, size.width)
   store.ArrIDs(size.height, size.width)
+  inputBtn.remove()
+  divInput.remove()
 }
 
 addShips.addEventListener('click', placementShips)
@@ -104,22 +119,32 @@ addShips.addEventListener('click', placementShips)
 function placementShips() {
   const btnPlaced = document.querySelector('button.placed')
 
-  btnPlaced.addEventListener('click', fieldHide)
   addShip()
+  btnPlaced.addEventListener('click', fieldHide)
+  addShips.remove()
 }
 
-function fieldHide() {
+function fieldHide(btnPlaced) {
+  const btn = document.querySelector('button.placed')
+
   const cells = document.querySelectorAll('i.fa-ship')
   cells.forEach(cell => shipToClear(cell))
 
+  const cellsPlaced = document.querySelectorAll('i.placed')
+  cellsPlaced.forEach(cel => cel.classList.remove('placed'))
+  addDelBtn(btn, false)
   shot()
 }
 
 function aim(battlefields) {
-  const clear = document.querySelectorAll('i.far.fa-square')
+  // const clear = document.querySelectorAll('i.far.fa-square')
 
   battlefields.addEventListener('mouseover', e => {
-    if (e.target.tagName === 'I') {
+    if (
+      e.target.tagName === 'I' &&
+      e.target.classList.contains('fa-square') &&
+      e.target.classList.contains('far')
+    ) {
       e.target.classList.remove('far')
       e.target.classList.remove('fa-square')
       e.target.classList.add('fas')
@@ -130,16 +155,19 @@ function aim(battlefields) {
     }
   })
   battlefields.addEventListener('mouseout', e => {
-    if (e.target.tagName === 'I') {
+    if (
+      e.target.tagName === 'I' &&
+      e.target.classList.contains('fa-crosshairs')
+    ) {
       e.target.classList.remove('fas')
       e.target.classList.remove('fa-crosshairs')
       e.target.classList.add('far')
       e.target.classList.add('fa-square')
 
-      // if (e.target.parentElement.classList.contains('btn-warning')) {
-      e.target.parentElement.classList.remove('btn-warning')
-      e.target.parentElement.classList.add('btn-light')
-      // }
+      if (e.target.parentElement.classList.contains('btn-warning')) {
+        e.target.parentElement.classList.remove('btn-warning')
+        e.target.parentElement.classList.add('btn-light')
+      }
     }
   })
 }
@@ -148,29 +176,32 @@ function addShip() {
   const battlefields = document.getElementById('battlefields')
 
   battlefields.addEventListener('click', e => {
-    if (e.target.tagName === 'I') {
-      e.target.classList.remove('far')
-      e.target.classList.remove('fa-square')
-      e.target.classList.remove('fa-2x')
-      e.target.classList.remove('clear')
+      if (
+        e.target.tagName === 'I' &&
+        e.target.classList.contains('placed')
+      ) {
+        e.target.classList.remove('far')
+        e.target.classList.remove('fa-square')
+        e.target.classList.remove('fa-2x')
+        e.target.classList.remove('clear')
 
-      e.target.classList.add('fas')
-      e.target.classList.add('fa-ship')
-      e.target.classList.add('fa-1x')
+        e.target.classList.add('fas')
+        e.target.classList.add('fa-ship')
+        e.target.classList.add('fa-1x')
 
-      store.mapId = e.target.id
-    }
+        store.mapId = e.target.id
+      }
   })
 }
 
-function addDelBtn(elem, tf) {
+function addDelBtn(elem, tf, text) {
   if (tf) {
     const addShipsBtn = document.createElement('button')
 
     addShipsBtn.classList.add('btn')
     addShipsBtn.classList.add('btn-success')
     addShipsBtn.classList.add('placed')
-    addShipsBtn.textContent = 'Готово'
+    addShipsBtn.textContent = text
 
     elem.push(addShipsBtn)
   }
@@ -192,6 +223,33 @@ function shipToClear(elem) {
 
 function shot(id) {
   const battlefields = document.getElementById('battlefields')
+
+  battlefields.addEventListener('click', e => {
+    if (
+      e.target.tagName === 'I' &&
+      e.target.parentElement.classList.contains('btn-warning')
+    ) {
+      if (store.map[e.target.id]) {
+        // Попадание в корабль
+        e.target.classList.remove('fas')
+        e.target.classList.remove('fa-crosshairs')
+        e.target.parentElement.classList.remove('btn-warning')
+
+        e.target.parentElement.classList.add('btn-danger')
+        e.target.classList.add('fas')
+        e.target.classList.add('fa-times')
+      } else {
+        // Промах
+        e.target.classList.remove('fas')
+        e.target.classList.remove('fa-crosshairs')
+        e.target.parentElement.classList.remove('btn-warning')
+
+        e.target.parentElement.classList.add('btn-light')
+        e.target.classList.add('fas')
+        e.target.classList.add('fa-square')
+      }
+    }
+  })
 
   aim(battlefields)
 }
